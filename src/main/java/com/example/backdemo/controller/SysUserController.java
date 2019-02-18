b72df8f893f4f44400aacccedbe6523447b695e3
SopLong <915257899@qq.com>
2019-02-18 20:47:59 +0800

    springboot整合mybatis之分页，redis依赖配置的添加

--- src/main/java/com/example/backdemo/controller/SysUserController.java
+++ src/main/java/com/example/backdemo/controller/SysUserController.java
@@ -1,18 +1,20 @@
 package com.example.backdemo.controller;
 
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.example.backdemo.pojo.SysUser;
 import com.example.backdemo.service.SysUserService;
 import com.example.backdemo.utils.IMoocJSONResult;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -30,14 +32,34 @@ public class SysUserController {
     private SysUserService sysUserService;
 
     @PostMapping("add")
+    @Transactional(propagation = Propagation.REQUIRED)
     public IMoocJSONResult add(){
         SysUser sysUser = new SysUser();
-        sysUser.setName("张三");
+        sysUser.setName("wangwu");
         sysUser.setAge(18);
         sysUser.setBirthday(new Date());
         sysUserService.insert(sysUser);
+        int i = 1/0;
         return new IMoocJSONResult();
     }
 
+    @PostMapping("update")
+    public IMoocJSONResult update(){
+        SysUser sysUser = new SysUser();
+        sysUser.setAge(20);
+        sysUser.setBirthday(new Date());
+        sysUserService.update(sysUser,new EntityWrapper<SysUser>().eq("name","张三"));
+        return new IMoocJSONResult();
+    }
+
+    @GetMapping("findAll")
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public IMoocJSONResult findAll(){
+        //分页的实现
+        PageHelper.startPage(1,20);
+
+        List<SysUser> sysUsers = sysUserService.selectList(new EntityWrapper<SysUser>());
+        return new IMoocJSONResult();
+    }
 }
 
